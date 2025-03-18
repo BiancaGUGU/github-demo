835622134e6d3e668378d2c8347ac4d97a17a38d
Gugu Bianca <gugu.bianca@yahoo.com>
2025-03-18 12:14:12 +0200

    Prmul commit pentru actualizarea proiectului movie rater

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import MovieList from'./components/movie-list.jsx';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Movie Rater </h1>
+        <div className='grid grid-cols-2'>
+            <MovieList/>
+            <h1>Movie Details</h1>
+        </div>
+     </header>
     </div>
   );
 }
